74bd5c6a55d59fb7dd88af83f150eafd84bd14da
likhithkp <likhithkpdev@gmail.com>
2024-07-01 10:22:22 +0530

    Practice

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-a = int(input("Enter the number"))
-print(f"The rem is a {a ** 2}")
+password = "qwerty"
+
+def get_passwd():
+    passwd = input("Enter the password")
+    while passwd != password:
+        print("Wrong password")
+        break
+
+get_passwd()
