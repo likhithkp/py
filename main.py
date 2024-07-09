546f1ff5e00022c87c21ba184d3ba93f76b4584b
likhithkp <likhithkpdev@gmail.com>
2024-07-09 08:53:29 +0530

    Sets

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-a = {"Harry" : 100, "Shiba": 70}
-a.popitem()
-print(a)
+words = {
+    "e" : "Help...!!",
+    "name":"lauda",
+    "nishanth":"Lauda1"
+}
+
+word = input("Enter a word")
+print(words[word])
