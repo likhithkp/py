a7503d598ec16cb7dc002a4bad1adb292b726813
likhithkp <likhithkpdev@gmail.com>
2024-07-10 06:07:07 +0530

    Practice

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-words = {
-    "e" : "Help...!!",
-    "name":"lauda",
-}
+age = int(input("Enter your age\t"))
 
-words.update({"hello" : "lauda"})
-print(words)
+if age > 18:
+    print("Welcome to the club")
+elif age == 18:
+    print("Perfect, welcome")
+elif age < 0:
+    print("Not a valid age")
+else:
+    print("Oops you are a child")
