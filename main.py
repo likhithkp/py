816cddbbca38c0f8064468e1033890aea2c70859
likhithkp <likhithkpdev@gmail.com>
2024-07-12 08:05:46 +0530

    PRactice

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-age = int(input("Enter your age\t"))
-
-if age > 18:
-    print("Welcome to the club")
-elif age == 18:
-    print("Perfect, welcome")
-elif age < 0:
-    print("Not a valid age")
-else:
-    print("Oops you are a child")
+i = 0
+while i == 0:
+    print("Hello")
