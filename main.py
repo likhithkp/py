59fcf3ee1dc1c4479c30b570bf4966387fa1c9f7
likhithkp <likhithkpdev@gmail.com>
2024-07-24 12:31:33 +0530

    Practice

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Practic")
+import os
+
+files = os.listdir()
+
+main_file_found = False
+
+for file in files:
+    if file == "main.py":
+        main_file_found = True
+        break
+
+if main_file_found:
+    print("File found")
+else:
+    print("No main file found")
